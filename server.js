93f8aa620b6d63d0b75198d4f6576bf13a3e3458
Muhammad Rashid <rashiddaha80@gmail.com>
2024-06-24 13:06:17 +0500

    final changes

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
-const server = http.createServer((req, res) => {
-    // Get the file path based on the request
-    const filePath = path.join(__dirname, 'index.html');
-    
-    // Read the file content
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        }
-    });
-});
-
-const PORT = 3000;
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server listening on port ${PORT}`);
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Welcome to my YouTube channel</h1><p>Hello, World!</p>');
+}).listen(3000, '0.0.0.0', () => {
+    console.log('Server listening on port 3000');
 });
